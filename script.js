7bef506bc92496692626c1c174ea8e8b20102876
midhun786 <105774970+midhun786@users.noreply.github.com>
2022-07-15 01:24:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+setTimeout(() => {
+    document.getElementById('test').innerHTML=10;
+    setTimeout(() => {
+        document.getElementById('test').innerHTML=9;
+        setTimeout(() => {
+            document.getElementById('test').innerHTML=8;
+            setTimeout(() => {
+                document.getElementById('test').innerHTML=7;
+                setTimeout(() => {
+                    document.getElementById('test').innerHTML=6;
+                    setTimeout(() => {
+                        document.getElementById('test').innerHTML=5;
+                        setTimeout(() => {
+                            document.getElementById('test').innerHTML=4;
+                            setTimeout(() => {
+                                document.getElementById('test').innerHTML=3;
+                                setTimeout(() => {
+                                    document.getElementById('test').innerHTML=2;
+                                    setTimeout(() => {
+                                        document.getElementById('test').innerHTML=1;
+                                        setTimeout(() => {
+                                            document.getElementById('test').innerHTML="<strong>HAPPY INDEPENDENCE DAY</strong>";
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
+    
